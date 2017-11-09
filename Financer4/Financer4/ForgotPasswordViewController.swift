2509b3327711a204184c971208271f22263ea8bb
Thiago <thiago@macintosh-3.local>
2017-11-09 21:32:29 -0200

    Outlet do relatório e outlet do Esqueci minha senha

---
+++ Financer4/Financer4/ForgotPasswordViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ForgotPasswordViewController.swift
+//  Financer4
+//
+//  Created by Thiago on 09/11/2017.
+//  Copyright © 2017 Thiago. All rights reserved.
+//
+
+import UIKit
+
+class ForgotPasswordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+
+}
